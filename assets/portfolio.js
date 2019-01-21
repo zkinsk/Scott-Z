7fa232617f712033bae8241049c67240ebc38edc
Zkinsk <zoolunk@gmail.com>
2019-01-20 23:46:55 -0500

    working iframe modal for responsive projects

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -1,8 +1,16 @@
 
-
+function modalMonitor(){
+  $(".modalProject").click(function(){
+    $("#portfolioModalContent").empty();
+    let projUrl = $(this).attr("data-url")
+    let frame = $("<iframe>").attr({"src": projUrl, "allow": "geolocation"})
+    $("#portfolioModalContent").append(frame);
+    $("#porfolioModal").modal();
+  })
+}
 
 $(document).ready(function(){
 
-  
+  modalMonitor()
 // end of doc ready
 })
