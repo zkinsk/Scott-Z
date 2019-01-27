6840cd4a81e89ab294d2fe6ee03cc620dec26db1
Zkinsk <zoolunk@gmail.com>
2019-01-26 19:06:56 -0500

    add firebase as destination for contact me messages

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -1,3 +1,15 @@
+var config = {
+  apiKey: "AIzaSyBJDXzudh6eMwDmTWJ-FHAUayeHrHYVTMg",
+  authDomain: "contact-me-a1f91.firebaseapp.com",
+  databaseURL: "https://contact-me-a1f91.firebaseio.com",
+  projectId: "contact-me-a1f91",
+  storageBucket: "contact-me-a1f91.appspot.com",
+  messagingSenderId: "708955501166"
+};
+firebase.initializeApp(config);
+
+var database =firebase.database();
+var messageDB = database.ref("/messages");
 
 function modalMonitor(){
   $(".modalProject").click(function(){
@@ -8,9 +20,28 @@ function modalMonitor(){
     $("#porfolioModal").modal();
   })
 }
+function clickButtons(){
+$("#contactModal").on("click", "#messageSend", function(){
+    let senderName = $("#sender-name").val();
+    let senderEmail = $("#sender-email").val();
+    let senderMessage = $("#sender-message").val();
+    setTimeout(function(){$('#contactModal').modal('hide', )}, 1000)
+    $("#sender-name, #sender-email, #sender-message").val("");
+    $("#sender-message").val("Thanks!");
+    $("#contactModal").on("hidden.bs.modal", function(){
+    $("#sender-message").val("");
+    })	
+    messageDB.push({
+      name: senderName,
+      senderEmail: senderEmail,
+      senderMessage: senderMessage,
+    })
+  })
+}
 
 $(document).ready(function(){
 
   modalMonitor()
+  clickButtons()
 // end of doc ready
 })
